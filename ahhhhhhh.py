ef5e6f666d4309bb305c139e92a4fea67fba8a5e
ud20667 <78478888+ud20667@users.noreply.github.com>
2022-12-08 11:19:17 +0000

    Update ahhhhhhh.py

--- ahhhhhhh.py
+++ ahhhhhhh.py
@@ -4,12 +4,16 @@ Created on Sun Dec  4 12:31:18 2022
 
 @author: rebec
 """
+# comment out = ctrl + 4
+# uncomment = crtl + 1
 
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
-import librosa
+#import librosa
 import soundfile as sf
+import scipy
 from scipy import signal
+import numpy as np
 
 cargoList       = [15,27,38,41,44,62,69,78,96,99,103,110]
 passengerList   = [1,4,5,6,8,9,12,14,16,17,23,27,29,30,31,32,33,37,41,50]
@@ -59,21 +63,45 @@ for boatNum in tugList:
 # tankerList      = [2,5,6,8,9,10,12,13,14,18,19,20,21,24,29,30,32,33,35,38,39,41,42,43,47,48,49,50]
 # tugList         = [9,40,49]
 # =============================================================================
+def plotSigSpec(boatNum,boatType):
+    samplerate, data = wavfile.read(f'Project_2/{boatType}/{boatNum}.wav')
+    plt.subplot(211)
+    plt.title(f'Spectrogram {boatType} {boatNum}')
+    plt.plot(data)
+    plt.xlabel('Sample')
+    plt.ylabel('Amplitude')
 
-samplerate, data = wavfile.read(f'Project_2/Cargo/15.wav')
+    plt.subplot(212)
+    plt.specgram(data,Fs=samplerate)
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
 
-plt.subplot(211)
-plt.title('Spectrogram of a wav file')
-plt.plot(data)
-plt.xlabel('Sample')
-plt.ylabel('Amplitude')
+    plt.show()
 
-plt.subplot(212)
-plt.specgram(data,Fs=samplerate)
-plt.xlabel('Time')
-plt.ylabel('Frequency')
 
-plt.show()
+
+def plotISI(boatNum,boatType,plotIdx):
+    samplerate, data = wavfile.read(f'Project_2/{boatType}/{boatNum}.wav')
+    ISI = np.diff(data)
+    plt.subplot(plotIdx)
+    plt.hist(ISI,bins=100)
+    plt.title(f"{boatType} {boatNum}")
+    plt.show()
+    kurt = scipy.stats.kurtosis(data)
+    print ("kurtosis = ", kurt)
+    
+# =============================================================================
+# plotISI(15,"Cargo",311) # normal all the way through
+# plotISI(27,"Cargo",312) # front heavy
+# plotISI(69,"Cargo",313) # back heavy ish
+# =============================================================================
+
+plotISI(38,"Cargo",111)
+plotSigSpec(38,"Cargo")
+
+
+
+
 
 
 
